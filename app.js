169407715b10fdbb0071b21fcab2939594a15d44
Adalberto <94541037+Adalberto15F@users.noreply.github.com>
2025-03-30 12:55:02 +0000

    criação do app.js e index.html

---
+++ app.js
@@ -0,0 +1,25 @@
+const app = document.getElementById("app")
+
+const formAction = () => {
+    const form = document.getElementById("form")
+    form.onsubmit = (event) => {
+    event.preventDefault()
+    alert("oi")
+    }
+}
+
+const startApp = () => {
+    const content =`
+        <form id="form">
+            <input type="email" name="email" placeholder="E-mail"/>
+            <input type="text" name="text" placeholder="Telefone"/>
+            <button>
+                Confirmar
+            </button>
+         </form>
+    `
+
+    app.innerHTML = content
+    startApp()
+
+}
